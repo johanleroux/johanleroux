bbcdc59e9fe9bd9af359a58cc4cdb7ef5b1eeb66
Johan le Roux <jleroux99@gmail.com>
2018-12-22 19:39:33 +0200

    Fix Nova Auth

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -43,10 +43,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewNova', function ($user) {
-            $emails = User::pluck('emails');
-            return in_array($user->email, [
-                $emails
-            ]);
+            $emails = User::pluck('email');
+            return in_array($user->email, $emails);
         });
     }
 
